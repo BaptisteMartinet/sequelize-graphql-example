6d200acde94c262df382ad09c3c6eb90c1bc9e18
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-05-09 17:37:40 +0200

    deps: setup prettier

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import schema from "./schema";
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import schema from './schema';
 
 async function main() {
   const server = new ApolloServer({ schema });
