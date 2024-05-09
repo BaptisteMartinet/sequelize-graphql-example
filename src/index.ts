cc7916731a9bf7d5c085e84843863be3f398ba71
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-05-09 17:29:53 +0200

    misc: setup graphql

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hello world')
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import schema from "./schema";
+
+async function main() {
+  const server = new ApolloServer({ schema });
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+  console.info(`Server ready at: ${url}`);
+}
+
+main().catch(console.error);
