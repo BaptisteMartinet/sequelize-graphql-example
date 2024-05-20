8ec0fc2500d1361df77cfb55be9734046ca84ae7
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-05-20 02:19:09 +0200

    [API] feat(schema/mutation): use genModelMutations

--- src/schema/mutation/AuthorMutation.ts
+++ src/schema/mutation/AuthorMutation.ts
@@ -1,29 +1,28 @@
-import { GraphQLInputObjectType, GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql';
+import { GraphQLNonNull, GraphQLString } from 'graphql';
+import { genModelMutations } from '@sequelize-graphql/core';
 import { Author } from '@models/index';
 
-export default new GraphQLObjectType({
-  name: 'AuthorMutation',
-  fields: {
-    create: {
-      type: new GraphQLNonNull(Author.type),
-      args: {
-        input: {
-          type: new GraphQLNonNull(
-            new GraphQLInputObjectType({
-              name: 'CreateAuthorInput',
-              fields: {
-                name: { type: new GraphQLNonNull(GraphQLString) },
-              },
-            }),
-          ),
-        },
-      },
-      async resolve(_, args, ctx) {
-        const {
-          input: { name },
-        } = args;
-        return Author.model.create({ name });
-      },
+export default genModelMutations(Author, {
+  create: {
+    args: {
+      name: { type: new GraphQLNonNull(GraphQLString) },
+    },
+    resolve(_, args, ctx) {
+      const { name } = args;
+      return Author.model.create({ name });
+    },
+  },
+  update: {
+    args: {
+      name: { type: GraphQLString },
+    },
+    resolve(author, args, ctx) {
+      return author.update({ name: args.name });
+    },
+  },
+  delete: {
+    resolve(author, args, ctx) {
+      return author.destroy();
     },
   },
 });
